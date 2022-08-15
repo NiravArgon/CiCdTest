1ac1970f36ae69fc610cdda08f90189ecbe71b32
NiravArgon <nirav.argon@gmail.com>
2022-08-15 17:51:31 +0530

    Test Changes

--- app/src/main/java/com/example/cicdtest/MainActivity.kt
+++ app/src/main/java/com/example/cicdtest/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.cicdtest
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.graphics.Color
+import androidx.core.content.ContextCompat
+import com.example.cicdtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+class MainActivity : ActBase<ActivityMainBinding>() {
+
+    override fun setViewBinding() = ActivityMainBinding.inflate(layoutInflater)
+
+    override fun bindObjects() {
+        
+    }
+
+    override fun bindListeners() {
+        binding.tabLayout.item1.setOnClickListener {
+            binding.tabLayout.select.animate().x(0f).duration = 100
+            binding.tabLayout.item1.setTextColor(Color.WHITE)
+            binding.tabLayout.item2.setTextColor(ContextCompat.getColor(this,R.color.appColor))
+        }
+
+        binding.tabLayout.item2.setOnClickListener {
+            binding.tabLayout.item1.setTextColor(ContextCompat.getColor(this,R.color.appColor))
+            binding.tabLayout.item2.setTextColor(Color.WHITE)
+            val size:Float = binding.tabLayout.item2.width.toFloat()
+            binding.tabLayout.select.animate().x(size).duration = 100
+        }
+    }
+
+    override fun bindMethods() {
+        
     }
 }
